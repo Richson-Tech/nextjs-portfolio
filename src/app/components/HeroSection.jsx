5753f3781280a4051ef47df0b7ccac86eb934016
Richson-Tech <folorunshoahmedolajide@gmail.com>
2024-04-14 12:54:53 +0100

    new commits

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -36,7 +36,7 @@ const HeroSection = () => {
               repeat={Infinity}
             />
           </h1>
-          <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl">
+          <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl lg:space-x-3">
             As an experienced front-end developer, I excel in the creation and
             refinement of web applications. Throughout my career, I've honed my
             expertise in various cutting-edge technologies such as React,
