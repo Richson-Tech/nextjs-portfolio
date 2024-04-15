bd8ca1904b77754f0ba96fd6dc4bb52c8af76bd3
Richson-Tech <folorunshoahmedolajide@gmail.com>
2024-04-15 12:36:13 +0100

    new commits

--- src/app/components/AboutSection.jsx
+++ src/app/components/AboutSection.jsx
@@ -57,12 +57,11 @@ const AboutSection = () => {
         <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
           <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
           <p className="text-base lg:text-lg">
-            I am a full stack web developer with a passion for creating
-            interactive and responsive web applications. I have experience
-            working with JavaScript, React, Redux, Node.js, Express, PostgreSQL,
-            Sequelize, HTML, CSS, and Git. I am a quick learner and I am always
-            looking to expand my knowledge and skill set. I am a team player and
-            I am excited to work with others to create amazing applications.
+            My line of Expertise I help in finding solutions and solve problems
+            with my technical skills, making the unreachable services available
+            at the click of a button.I also provide technical services for
+            brands and work for companies to fix world problems with the use of
+            my technical experience.
           </p>
           <div className="flex flex-row justify-start mt-8">
             <TabButton
