82253f3effd72ef1232f187cfc130c42994a4afb
Richson-Tech <folorunshoahmedolajide@gmail.com>
2024-04-15 14:33:57 +0100

    new commits

--- src/app/components/ProjectsSection.jsx
+++ src/app/components/ProjectsSection.jsx
@@ -7,7 +7,7 @@ import { motion, useInView } from "framer-motion";
 const projectsData = [
   {
     id: 1,
-    title: "React Nike Website",
+    title: "E-commerce Website",
     description: "Project 1 description",
     image: "/images/projects/1.png",
     tag: ["All", "dynamic"],
