36a3cea6bc605de358817439279dcfc8d9dc7e46
Richson-Tech <folorunshoahmedolajide@gmail.com>
2024-04-15 14:59:30 +0100

    new commits

--- src/app/components/ProjectsSection.jsx
+++ src/app/components/ProjectsSection.jsx
@@ -27,7 +27,7 @@ const projectsData = [
   {
     id: 3,
     title: "Password generator Application",
-    description: "Project 3 description",
+    description: "The Password generator utillizes the information the user provides, such as your date of birth and full-name, to craft unique and robust passwords tailored to you. By combining these personal details with advanced algorithms, it generate random passwords that are virtually impossible to guess yet easy to remember. ",
     image: "/images/projects/3.png",
     tag: ["All", "dynamic"],
     gitUrl: "/",
