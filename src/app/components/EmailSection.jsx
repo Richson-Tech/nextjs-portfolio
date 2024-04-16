991eed0c6f325097020b2428206effc19f16bf83
Richson-Tech <folorunshoahmedolajide@gmail.com>
2024-04-16 18:13:52 +0100

    new commit

--- src/app/components/EmailSection.jsx
+++ src/app/components/EmailSection.jsx
@@ -55,7 +55,7 @@ const EmailSection = () => {
           {" "}
           I&apos;m currently looking for new opportunities, my inbox is always
           open. Whether you have a question or just want to say hi, I&apos;ll
-          try my best to get back to you!
+          surely get back to you!
         </p>
         <div className="socials flex flex-row gap-2">
           <Link href="github.com">
