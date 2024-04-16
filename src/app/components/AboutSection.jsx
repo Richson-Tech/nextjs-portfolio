5d7f00692eb0ab13c488f25ab27b4589af19c507
Richson-Tech <folorunshoahmedolajide@gmail.com>
2024-04-16 16:09:54 +0100

    new commit

--- src/app/components/AboutSection.jsx
+++ src/app/components/AboutSection.jsx
@@ -43,39 +43,70 @@ const AboutSection = () => {
 
   return (
     <section className="text-white" id="about">
-      <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-1p sm:py-16 xl:px-16">
-        <div className="mt-1 md:mt-0 text-left flex flex-col h-full">
-          <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
-          <p className="text-base lg:text-lg">
-            My line of Expertise I help in finding solutions and solve problems
-            with my technical skills, making the unreachable services available
-            at the click of a button.I also provide technical services for
-            brands and work for companies to fix world problems with the use of
-            my technical experience.
-          </p>
-          <div className="flex flex-row justify-between mt-8">
-            <TabButton
-              selectTab={() => handleTabChange("skills")}
-              active={tab === "skills"}
-            >
-              {" "}
-              Skills{" "}
-            </TabButton>
-            <TabButton
-              selectTab={() => handleTabChange("education")}
-              active={tab === "education"}
-            >
-              {" "}
-              Education{" "}
-            </TabButton>
-        
-          </div>
-          <div className="mt-8">
-            {TAB_DATA.find((t) => t.id === tab).content}
-          </div>
-        </div>
+  <div className="md:flex justify-between py-8 px-4 xl:gap-1p sm:py-16 xl:px-16">
+    {/* First About Me Part */}
+    <div className="md:w-1/2 mt-1 md:mt-0 text-left flex flex-col">
+      <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
+      <p className="text-base lg:text-lg">
+        My line of Expertise I help in finding solutions and solve problems
+        with my technical skills, making the unreachable services available
+        at the click of a button.I also provide technical services for
+        brands and work for companies to fix world problems with the use of
+        my technical experience.
+      </p>
+      <div className="flex flex-row justify-between mt-8">
+        <TabButton
+          selectTab={() => handleTabChange("skills")}
+          active={tab === "skills"}
+        >
+          {" "}
+          Skills{" "}
+        </TabButton>
+        <TabButton
+          selectTab={() => handleTabChange("education")}
+          active={tab === "education"}
+        >
+          {" "}
+          Education{" "}
+        </TabButton>
       </div>
-    </section>
+      <div className="mt-8">
+        {TAB_DATA.find((t) => t.id === tab).content}
+      </div>
+    </div>
+    {/* Second About Me Part */}
+    <div className="md:w-1/2 mt-1 md:mt-0 text-left flex flex-col">
+      <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
+      <p className="text-base lg:text-lg">
+        My line of Expertise I help in finding solutions and solve problems
+        with my technical skills, making the unreachable services available
+        at the click of a button.I also provide technical services for
+        brands and work for companies to fix world problems with the use of
+        my technical experience.
+      </p>
+      <div className="flex flex-row justify-between mt-8">
+        <TabButton
+          selectTab={() => handleTabChange("skills")}
+          active={tab === "skills"}
+        >
+          {" "}
+          Skills{" "}
+        </TabButton>
+        <TabButton
+          selectTab={() => handleTabChange("education")}
+          active={tab === "education"}
+        >
+          {" "}
+          Education{" "}
+        </TabButton>
+      </div>
+      <div className="mt-8">
+        {TAB_DATA.find((t) => t.id === tab).content}
+      </div>
+    </div>
+  </div>
+</section>
+
   );
 };
 
