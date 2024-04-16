398b06918d829d919ab18f3d8332b0875b7b180b
Richson-Tech <folorunshoahmedolajide@gmail.com>
2024-04-16 18:28:26 +0100

    new commit

--- src/app/components/EmailSection.jsx
+++ src/app/components/EmailSection.jsx
@@ -127,7 +127,7 @@ const EmailSection = () => {
             </div>
             <button
               type="submit"
-              className=" bg-secondary-500 hover:bg-blue-700 text-white font-medium py-2.5 px-5 rounded-lg w-full"
+              className=" bg-secondary-500 hover:bg-blue-00 text-white font-medium py-2.5 px-5 rounded-lg w-full"
             >
               Send Message
             </button>
--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className="footer border z-10 border-t-[#33353F] border-l-transparent border-r-transparent text-white">
       <div className="container p-12 flex justify-between">
-        <span>FA</span>
+        <span>F<span className="text-secondary-600">A</span></span>
         <p className="text-white">&copy; All rights reserved.</p>
       </div>
     </footer>
