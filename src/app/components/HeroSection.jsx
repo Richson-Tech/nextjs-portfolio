76826e329be336893168f5546565e50aa8dc8b75
Richson-Tech <folorunshoahmedolajide@gmail.com>
2024-04-21 15:22:27 +0100

    new commits

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -61,7 +61,7 @@ const HeroSection = () => {
           transition={{ duration: 0.5 }}
           className="col-span-4 place-self-center mt-4 lg:mt-0"
         >
-          <div className="rounded-full mt-10 bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
+          <div className="mt-10 w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
             <Image
               src="/images/me.jpg"
               alt="Ahmed"
