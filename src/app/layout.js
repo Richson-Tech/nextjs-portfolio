0cb57417e2e2868f74cbb6c7f4b9f950967cb5a7
Richson-Tech <folorunshoahmedolajide@gmail.com>
2024-09-10 01:08:28 +0100

    new commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Ahmed Portfolio",
+  title: "Ahmed's Portfolio",
   description: "Folorunsho Ahmed's Portfolio",
 };
 
